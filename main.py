c05878f3f964385d2495677cb760b07b8d62b8ab
mole828 <mrx800@163.com>
2024-04-16 18:19:01 +0800

    add: session check

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ sessions = os.environ.get(key='SESSIONS',default='').split(',')
 if __name__ == '__main__': 
     print("begin checkin")
     for session in sessions:
-        user = User(session) 
-        print('session:',session[:3], '签到结果:', user.checkin().__dict__) 
+        if session:
+            user = User(session) 
+            print('session:',session[:3], '签到结果:', user.checkin().__dict__) 
     print("finish checkin")
